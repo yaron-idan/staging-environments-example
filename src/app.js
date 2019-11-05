af116933f67d2e238903387e59756bc4a55e7bb0
Yaron Idan <yaron@soluto.com>
2019-11-05 16:38:52 +0200

    make app js fail healthcheck

--- src/app.js
+++ src/app.js
@@ -2,14 +2,29 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
-  fs.readFile(__dirname + '/files/index.html', function (err,data) 
+  console.log(req.url);
+  if (req.url === '/healthcheck')
+  {
+    res.writeHead(200);
+    res.end("healthy");
+  }
+  else if (req.url === '/index.html')
+  {
+    fs.readFile(__dirname + '/files/index.html', function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+      res.writeHead(200);
+      res.end(data);
+    });
+  }
+  else
   {
-  if (err) {
     res.writeHead(404);
-    res.end(JSON.stringify(err));
+    res.end("NOT FOUND");
     return;
   }
-    res.writeHead(200);
-    res.end(data);
-  });
+  
 }).listen(8080);
