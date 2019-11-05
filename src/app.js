814cfb09e12efd44cb59429a0a92f01b7857bf5f
Yaron Idan <yaron@yaron-mac.soluto.local>
2019-11-05 13:03:11 +0200

    add newline

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
-  fs.readFile(__dirname + '/files/index.html', function (err,data) {
+  fs.readFile(__dirname + '/files/index.html', function (err,data) 
+  {
   if (err) {
     res.writeHead(404);
     res.end(JSON.stringify(err));
