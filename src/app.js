4a87fb90ae6f89cb92497dc7aa3113011bba66df
Yaron Idan <yaron@soluto.com>
2019-11-06 11:14:46 +0200

    indent

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
-        fs.readFile(__dirname + '/files/index.html', function (err,data) {
-    if (err) {
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
-    }
-      res.writeHead(200);
-      res.end(data);
-    });  
+    fs.readFile(__dirname + '/files/index.html', function (err,data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+        res.writeHead(200);
+        res.end(data);
+      });  
 }).listen(8080);
